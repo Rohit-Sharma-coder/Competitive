0a9ceae5ad1527b14c0125f5e9381e4bd0128526
Rohit-sharma-coder <me8urohit@gmail.com>
2023-03-28 13:44:58 +0530

    BS

--- books.cpp
+++ books.cpp
@@ -4,11 +4,31 @@ using namespace std;
 #define vi vector<int> ;
 #define vii vector<vector<int> > ; 
 
+int bs(int *arr ,int n ,int element ){
+	int s =0,e=n;
+	while(s<e){
+	int mid = (s+e)/2;
+	if(element == arr[mid]){
+      	return mid; 
+	}
+	else if(arr[mid]<element){
+		s = mid+1;
+    }
+    else{
+    	e =mid-1;
+    }
+}
+    return s ;
 
+}
 
 
 void solve(){
-
+ int n , k;
+ cin>>n >> k;
+ int arr[n];
+ for(int i=0;i<n;i++) cin>>arr[i];
+ 	cout << bs(arr,k,n) << endl;
 }
 
 
